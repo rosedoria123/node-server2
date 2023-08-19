c06a60e928d46896bf87fd41dc0c8c5016ea2c4b
rosedoria123 <113267533+rosedoria123@users.noreply.github.com>
2023-08-19 11:11:55 -0500

    Add files via upload

---
+++ routes/routes/partnerRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const partnerRouter = express.Router();
+
+const Partner = require('../models/partner');
+
+partnerRouter.route('/')
+.get((req, res, next) => {
+    Partner.find()
+    .then(partners => res.status(200).json(partners))
+    .catch(err => next(err))
+})
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(partners => res.status(200).json(partners))
+    .catch(err => next(err))
+});
+
+partnerRouter.route('/:partnerId')
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+})
+.post((req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
+})
+.put((req, res, next) => {
+    Partner.findByIdAndUpdate(req.params.partnerId, req.body, { new: true })
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+})
+.delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+});
+
+module.exports = partnerRouter;
